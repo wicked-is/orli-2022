5733365f96eb90c1237d1b9acb25ad010815ce31
Kathryn Reina <kcreina@me.com>
2022-03-15 15:03:25 -0700

    added components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRef, useEffect } from 'react';
 import styles from '../styles/Home.module.css'
+import Hero from '../components/hero';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   return (
