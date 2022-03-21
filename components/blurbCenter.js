1a0aa715173b47676deb6ceb14e8aab4fc95b8a8
kennethtaylor <taylor.kennethg@gmail.com>
2022-03-21 10:48:03 -0700

    Errors fixed.

--- components/blurbCenter.js
+++ components/blurbCenter.js
@@ -3,13 +3,13 @@ import styles from '../styles/blurbCenter.module.css';
 export default function BlurbCenter({ content, icon }) {
     return (
         <section className={styles.blurbContainer}>
-            <div class="container">
-                <div class="row justify-content-center align-items-center">
-                    <div class="copy text-center">
-                        <div class="icon">
+            <div className="container">
+                <div className="row justify-content-center align-items-center">
+                    <div className="copy text-center">
+                        <div className="icon">
                             <img src={icon} alt="Shell icon" />
                         </div>
-                        <p class="large">
+                        <p className="large">
                             { content }
                         </p>
                     </div>
--- components/fauxSocialFeed.js
+++ components/fauxSocialFeed.js
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import styles from '../styles/fauxSocialFeed.module.css'
 
 export default function FauxSocialFeed() {
     return (
         <section className={styles.fauxSocialFeedContainer}>
             <h2>Follow Along</h2>
-            <a href="/">@stayorli</a>
+            <Link href="/">
+                <a>@stayorli</a>
+            </Link>
             <div>
                 <img src="https://orlidev.wpengine.com/wp-content/uploads/2022/01/Orli_Interior-1Web.jpg" />
                 <img src="https://orlidev.wpengine.com/wp-content/uploads/2022/01/Orli_Interior-1Web.jpg" />
