e5be7d0d7327e5bd0233cce9cab42d0b24420c89
kennethtaylor <taylor.kennethg@gmail.com>
2022-03-21 07:07:05 -0700

    slider logic

--- components/roomSlider.js
+++ components/roomSlider.js
@@ -1,12 +1,20 @@
+import { useRef } from 'react';
 import Image from 'next/image'
 import Link from 'next/link';
 import Flickity from 'react-flickity-component'
+    
 import styles from '../styles/roomSlider.module.css'
 
 import "flickity/css/flickity.css";
-import "flickity-as-nav-for"
 
 export default function RoomSlider({ rooms }) {
+
+    const slider = useRef(null)
+    const sliderNavigation = useRef(null)
+
+    const changeSlider = (e) => {
+        slider.current.select(e.target.dataset.slide)
+    }
     
     return (
         <section className={styles.roomSlider}>
@@ -24,42 +32,33 @@ export default function RoomSlider({ rooms }) {
                 disableImagesLoaded={false} // default false
                 reloadOnUpdate // default false
                 static // default false
-                flickityRef={c => this.flktyA = c}
+                flickityRef={c => {
+                    slider.current = c
+                }}
             >
                 {
                     rooms.map(room => {
                         return (
-                            <div className={styles.room}>
-                                
+                            <div key={room.title} className={styles.room}>
                                 <Image src={room.featuredImage.node.sourceUrl} alt={room.title} width={1436} height={1020} layout="intrinsic" />
                             </div>
                         )
                     })
                 }
             </Flickity>
-            <div className={styles.roomSliderNavigation} >
-                <Flickity
-                    options={{
-                        prevNextButtons: false,
-                        pageDots: false,
-                        draggable: false,
-                        wrapAround: true,
-                        asNavFor: this.flktyA
-                    }}
-                    flickityRef={c => this.flktyB = c}
-                >
-                    {
-                        rooms.map(room => {
-                            return (
-                                <p>
-                                    <a>
-                                        {room.title}
-                                    </a>
-                                </p>
-                            )
-                        })
-                    }
-                </Flickity>
+            <div className={styles.roomSliderNavigation}>
+                {
+                    rooms.map((room, index) => {
+                        let slideNum = index
+                        return (
+                            <p key={room.title} style={{ margin: '0 1rem' }}>
+                                <a data-slide={index} onClick={changeSlider}>
+                                    {room.title}
+                                </a>
+                            </p>
+                        )
+                    })
+                }
             </div>
         </section>
     )
