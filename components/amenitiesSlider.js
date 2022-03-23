4edccc48b9a449afeca095489c7a416bd31cf0bd
Kathryn Reina <kcreina@me.com>
2022-03-23 11:30:16 -0700

    amenities slider updates

--- components/amenitiesSlider.js
+++ components/amenitiesSlider.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Flickity from 'react-flickity-component'
 import "flickity/css/flickity.css";
@@ -5,6 +6,20 @@ import "flickity/css/flickity.css";
 import styles from '../styles/amenitiesSlider.module.css';
 
 export default function AmenitiesSlider({ amenities, subHeader, header, ctaText, ctaLink }) {
+    const slider = useRef(null)
+    const [sliderActive, setSliderActive] = useState(0)
+    const [loaded, setLoaded] = useState(false)
+
+    const changeSlider = (e) => {
+        slider.current.select(e.target.dataset.slide)
+    }
+
+    useEffect(() => {
+        slider.current.on('change', () => {
+            setSliderActive(slider.current.selectedIndex)
+        })
+    }, [sliderActive]);
+
     return (
         <section className={styles.amenitiesSlider} >
             <Flickity
@@ -14,33 +29,40 @@ export default function AmenitiesSlider({ amenities, subHeader, header, ctaText,
                     pageDots: false,
                     draggable: true,
                     wrapAround: true,
-                    autoPlay: 7000
+                    imagesLoaded: true,
                 }}
                 disableImagesLoaded={false} // default false
-                reloadOnUpdate // default false
+                reloadOnUpdate={false} // default false
                 static // default false
+                flickityRef={c => {
+                    slider.current = c
+                }}
             >
                 {
-                    amenities.map(amenity => {
+                    amenities.map((amenity, index) => {
                         return (
                             <div key={amenity.title} className={styles.amenity}>
                                 <Image src={amenity.featuredImage.node.sourceUrl} alt={amenity.title} width={1435} height={928} layout="intrinsic" />
+                           
+                            <div className={`${styles.sliderContent} ${ sliderActive == index ? styles.showcontent : '' }`}>
+                                <p className="sans-serif-bold sub-heading white">{subHeader}</p>
+                                <p className="heading white" style={{ margin: 0 }}>{header}</p>
+                                <p className="sans-serif xs-copy white" style={{ textDecoration: 'underline'}}><a href={ctaLink}>{ctaText}</a></p>
+                            </div>
                             </div>
                         )
                     })
                 }
             </Flickity>
-            <div className={styles.sliderContent}>
-                <p className="sans-serif-bold sub-heading white">{subHeader}</p>
-                <p className="heading white" style={{ margin: 0 }}>{header}</p>
-                <p className="sans-serif xs-copy white" style={{ textDecoration: 'underline'}}><a href={ctaLink}>{ctaText}</a></p>
-            </div>
+            
             <div className={styles.sliderNav}>
                 {
-                    amenities.map(amenity => {
+                    amenities.map((amenity, index) => {
                         return (
-                            <p key={`${amenity.title}-nav`} className={styles.navItem}>
-                                {amenity.title}
+                            <p key={`${amenity.title}-nav`} className={`${ sliderActive == index ? styles.active : '' }`} style={{ margin: '0 1rem' }}>
+                                <a className={styles.navItem} data-slide={index} onClick={changeSlider}>
+                                    {amenity.title}
+                                </a>
                             </p>
                         )
                     })
