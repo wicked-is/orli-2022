e38b1af4791068029b11b78251b064dd33459801
Kathryn Reina <kcreina@me.com>
2022-03-22 18:00:00 -0700

    font edits

--- components/eventFeed.js
+++ components/eventFeed.js
@@ -4,15 +4,15 @@ export default function EventFeed({ events }) {
     return (
         <>
             <article style={{ borderTop: '1px solid black' }}>
-                <p>Event Category | 01 March 2022</p>
+                <p className="sans-serif xs-copy left">Event Category | 01 March 2022</p>
                 <h3 className="heading" style={{ margin: '0 0 1rem' }}>Wedding Reception<span className={styles.arrow}></span></h3>
             </article>
             <article style={{ borderTop: '1px solid black' }}>
-                <p>Event Category | 01 March 2022</p>
+                <p className="sans-serif xs-copy left">Event Category | 01 March 2022</p>
                 <h3 className="heading" style={{ margin: '0 0 1rem' }}>Wedding Reception<span className={styles.arrow}></span></h3>
             </article>
             <article style={{ borderTop: '1px solid black' }}>
-                <p>Event Category | 01 March 2022</p>
+                <p className="sans-serif xs-copy left">Event Category | 01 March 2022</p>
                 <h3 className="heading" style={{ margin: '0 0 1rem' }}>Wedding Reception<span className={styles.arrow}></span></h3>
             </article>
         </>
