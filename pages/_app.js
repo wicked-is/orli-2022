da38a0dc5bd9ce7daa279ab9d8be33a48538d8a8
kennethtaylor <taylor.kennethg@gmail.com>
2022-04-27 15:23:33 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout'
 
 function MyApp(props) {
   
-  const navItems = props.pageProps.data.data.myOptionsPage.options.navigation.navigationItems
   const { Component, pageProps } = props
+  const navItems = pageProps.data?.data?.myOptionsPage?.options?.navigation?.navigationItems
 
   return (
       <Layout navItems={navItems}>
