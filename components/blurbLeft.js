f28b12d7749d67f8e36a0c767e5d61639fd354f1
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-05-11 09:16:11 -0700

    Mobile Edits

--- components/blurbLeft.js
+++ components/blurbLeft.js
@@ -6,7 +6,7 @@ export default function BlurbLeft({ content, ctaLink, ctaText, icon, title }) {
     return (
         <div className={styles.blurbLeftContainer}>
             {icon && <div className={styles.icon}><Image src={icon.mediaItemUrl} width={100} height={100} layout="fixed" alt={icon.altText} /></div>}
-            {title && <p className="sans-serif-bold sub-heading">{title}</p>}
+            {title && <p className={`${styles.mobilepadding} sans-serif-bold sub-heading`}>{title}</p>}
             {content && <p className={`${styles.content} heading`}>{content}</p>}
             {ctaLink && <p className={`${styles.cta} sans-serif xs-copy`}><a href={ctaLink}>{ ctaText }</a></p>}
         </div>
--- components/ourMission.js
+++ components/ourMission.js
@@ -28,15 +28,16 @@ export default function OurMission(props) {
     return (
         <section className={`${styles.missionContainer} center bg-lt-grey`}>
             <p className="serif sub-heading center brown">{title}</p>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', padding: '7rem 0' }}>
-                <Image src={logo.mediaItemUrl} alt={logo.altText} width={167} height={108} /><span className="heading" style={{
+            <div className={styles}>
+                <Image src={logo.mediaItemUrl} alt={logo.altText} width={167} height={108} />
+                <span className="heading" style={{
                     alignSelf: 'center', marginLeft: '2rem'
                 }}>
                     <span data-word={animatableTexts[0].title} className="textAnimation"></span> 
                     <span data-word={animatableTexts[1].title} className="textAnimation2"></span>
                     <span data-word={animatableTexts[2].title} className="textAnimation3"></span>
                     <span className="cursor"></span>
-                    </span>
+                </span>
             </div>
         </section>
     )
