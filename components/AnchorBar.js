ddaf95480ab63266935182ef5e41f4d9304be723
kennethtaylor <taylor.kennethg@gmail.com>
2022-05-18 13:57:09 -0700

    feat: Added standalone anchor bar

--- components/AnchorBar.js
+++ components/AnchorBar.js
@@ -1,24 +1,61 @@
-import styles from '../styles/hero.module.css'
+// import styles from '../styles/hero.module.css'
+import styled from 'styled-components'
+
+const SubNavContainer = styled.ul`
+    width: 100%;
+    background: var(--lt-grey);
+    padding: 1rem 0rem 0rem;
+    margin: 0;
+
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    list-style: none;
+
+    p {
+        font-family: 'GT Walsheim'; 
+        font-weight: 300;
+
+        &:hover {
+            font-family: 'GT Walsheim'; 
+            font-weight: 700;
+        }
+    }
+
+    li {
+        position: relative; 
+        margin: auto 2rem; 
+        cursor: pointer; 
+        transition: 0.3s ease all;
+    }
+
+    .iconNav {
+        width: 100%; 
+        max-width: 4rem;
+        margin: auto; 
+        text-align: center; 
+        display: block;
+    }
+`
 
 export default function AnchorBar(props) {
-    console.log(props);
+
+    const { anchorNavigation } = props
+
     return (
-        <ul className={styles.subnavigationContainer}>               
-            {/* {
-                // This was the incorrect bit. You were calling parameters
-                // on the item without having the item defined. 
-                // When you're back we can huddle if you'd like.
-                subnavigation.map((item, index) => {
+        <SubNavContainer>               
+            {   
+                anchorNavigation.map((item, index) => {
                     return (
                         <li key={`ni-${index}`} >
-                            <a href={item.link}>
-                            <img src={item.iconnav !== null ? item.iconnav.mediaItemUrl : 'https://orlidev.wpengine.com/wp-content/uploads/2022/03/Orli_agave-1.svg'} className={styles.iconnav} alt={item.iconnav?.altText} />
-                            <p className="black xs-copy uppercase center">{item.label}</p>
+                            <a href={item.anchor}>
+                            <img src={item.icon !== null ? item.icon.mediaItemUrl : 'https://orlidev.wpengine.com/wp-content/uploads/2022/03/Orli_agave-1.svg'} className="iconnav" alt={item.iconnav?.altText} />
+                            <p className="black xs-copy uppercase center">{item.text}</p>
                             </a>
                         </li>
                         )
                 })
-            } */}
-        </ul>
+            }
+        </SubNavContainer>
     )
 }
