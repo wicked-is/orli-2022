49bb1710e6a96ff918239e3f8a3e7751f11d9ee1
kennethtaylor <taylor.kennethg@gmail.com>
2022-05-25 15:10:35 -0700

    feat: added conditional styling

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -1,16 +1,22 @@
 import Image from 'next/image'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const TitleBarContainer = styled.section`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    
-    max-width: 80%;
 
-    margin-block: 2rem;
+    max-width: 80%;
     margin-inline: auto;
+    
+    margin-block: 2rem;
     padding: 2rem 1rem;
+
+    ${props => props.left && css`
+        margin-inline: initial !important;
+        max-width: 100%; 
+        padding: 0;
+    `}
 `
 
 export default function TitleBar(props) {
@@ -18,7 +24,7 @@ export default function TitleBar(props) {
     return (
         <TitleBarContainer>
             { icon && <Image src={icon.mediaItemUrl} alt={icon.altText} width="100px" height="100px" layout="fixed" /> }
-            <h1>{ title }</h1>
+            <h1 className="heading">{ title }</h1>
             { icon && <Image src={icon.mediaItemUrl} alt={icon.altText}  width="100px" height="100px" layout="fixed" /> }
         </TitleBarContainer>
     )
