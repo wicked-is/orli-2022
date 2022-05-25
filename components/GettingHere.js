4c468790bd143e3a0a18516ceaa2329b9b42d4ce
kennethtaylor <taylor.kennethg@gmail.com>
2022-05-25 16:19:40 -0700

    feat: updated to be dynamic

--- components/GettingHere.js
+++ components/GettingHere.js
@@ -2,12 +2,17 @@ import styled, { css } from "styled-components"
 
 import TitleBar from "./TitleBar"
 
-const GettingHereContainer = styled.section``
+const GettingHereContainer = styled.section`
+    display: flex;
+    max-width: 80%;
+    margin-inline: auto;
+`
 
 export default function GettingHere(props) {
+    const { title, blurb, image } = props
     return (
         <GettingHereContainer>
-            <TitleBar title="Getting Here" left/>
+            <TitleBar title={title} blurb={blurb} left/>
         </GettingHereContainer>
     )
 }
