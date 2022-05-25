cd1322e11c3c675b02dc7aa22cdc63e39477c0a2
kennethtaylor <taylor.kennethg@gmail.com>
2022-05-24 17:00:36 -0700

    Made icons conditional

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -17,9 +17,9 @@ export default function TitleBar(props) {
     const { title, icon } = props
     return (
         <TitleBarContainer>
-            <Image src={icon.mediaItemUrl} alt={icon.altText} width="100px" height="100px" layout="fixed" />
+            { icon && <Image src={icon.mediaItemUrl} alt={icon.altText} width="100px" height="100px" layout="fixed" /> }
             <h1>{ title }</h1>
-            <Image src={icon.mediaItemUrl} alt={icon.altText}  width="100px" height="100px" layout="fixed" />
+            { icon && <Image src={icon.mediaItemUrl} alt={icon.altText}  width="100px" height="100px" layout="fixed" /> }
         </TitleBarContainer>
     )
 }
