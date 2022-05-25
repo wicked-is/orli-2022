09566e9fe951a77236f0fddfe0a923e0f93b8c41
kennethtaylor <taylor.kennethg@gmail.com>
2022-05-24 17:00:25 -0700

    incorporating video background

--- components/blurbCenter.js
+++ components/blurbCenter.js
@@ -13,13 +13,23 @@ export default function BlurbCenter(props) {
         webm
     } = props
 
+    console.log(props);
+
     return (
-        <section className={`${styles.blurbContainer} ${greyBackground && 'bg-lt-grey'}`}>
-            <div className="max-75 center">
-                <div className={`${styles.icon}`}>
+        <section className={`${styles.blurbContainer} ${greyBackground && 'bg-lt-grey'} ${hasBackgroundMedia && styles.backgroundMedia}`} style={{ backgroundImage: `${imagePoster}` }}>
+            {
+                hasBackgroundMedia && mp4ExternalLink && (
+                    <video className={styles.backgroundMedia} autoPlay loop muted playsInline>
+                        <source src={mp4ExternalLink} type="video/mp4" />
+                        {webm && <source src={webm} type="video/webm" />}
+                    </video>
+                )
+            }
+            <div className={`max-75 center ${hasBackgroundMedia && styles.overContent} ${hasBackgroundMedia && 'white'}`}>
+                { icon && <div className={`${styles.icon}`}>
                     <img src={icon.mediaItemUrl} alt={icon.altText}/>
-                </div>
-                <p className="heading-italic">
+                </div> }
+                <p className="heading-italic white">
                     { blurb }
                 </p>
             </div>
