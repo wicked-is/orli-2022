5b5af67c25e927351b485b04cc386c96c3f949a9
kennethtaylor <taylor.kennethg@gmail.com>
2022-05-24 17:00:14 -0700

    Added janky debug

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/globals.css'
 import Layout from '../components/layout'
 
 function MyApp(props) {
+  // Show query errors
+  // console.log(props.pageProps.data.errors);
+  
   const { Component, pageProps } = props
   const navItems = props.pageProps?.data?.data?.myOptionsPage.options.navigation.navigationItems || null;
   const { announcementBar } = props.pageProps?.data?.data?.myOptionsPage.options || null;
