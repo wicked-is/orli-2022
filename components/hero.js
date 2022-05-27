c3492a220cbf2973c11e637f5f556bf8bbac310d
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-05-26 21:16:53 -0700

    ToggleActive State on SubNav

--- components/hero.js
+++ components/hero.js
@@ -2,6 +2,7 @@ import BookingForm from './bookingForm';
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/hero.module.css'
+import { useState } from 'react';
 
 export default function Hero(props) {
     
@@ -55,26 +56,40 @@ export default function Hero(props) {
                     </div>
                 )
             case 'Full Height Image with SubNav':
+                const [appState, changeState] = useState({
+                    activeObject: null,
+                    objects: [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7}, {id: 8}]
+                });
+
+                function toggleActive(index){
+                    changeState({...appState, activeObject: appState.objects[index] });
+                }
+
+                function toggleActiveStyles(index){
+                    if(appState.objects[index] === appState.activeObject){
+                        return "active";
+                    } else {
+                        return "inactive";
+                    }
+                }
                     return (
                     <div className={styles.heroContainer}>
                         <div className={styles.bigheroSubnav}>
                             <ul className={styles.subnavigationContainer}>
-                           
+                                
                                 {
-                                    // This was the incorrect bit. You were calling parameters
-                                    // on the item without having the item defined. 
-                                    // When you're back we can huddle if you'd like.
-                                    subnavigation.map((item, index) => {
-                                        return (
-                                            <li key={`ni-${index}`} >
+                                    subnavigation.map((item, index) => (
+                                     
+                                            <li key={`ni-${index}`} className={toggleActiveStyles(index)} onClick={() => {toggleActive(index);}}>
                                                 <a href={item.link}>
                                                 <img src={item.iconnav !== null ? item.iconnav.mediaItemUrl : 'https://orlidev.wpengine.com/wp-content/uploads/2022/03/Orli_agave-1.svg'} className={styles.iconnav} alt={item.iconnav?.altText} />
-                                                <p className="black xs-copy uppercase center">{item.label}</p>
+                                                <p className={`black xs-copy uppercase center`}>{item.label}</p>
                                                 </a>
                                             </li>
-                                            )
-                                    })
+                                    
+                                    ))
                                 }
+                                
                             </ul>
                         <div className={styles.bigHero} style={{
                             backgroundImage: `url(${imagePoster.mediaItemUrl})`
