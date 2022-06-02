83703f36dcf81ae0616a61b8b3972d40018e4258
kennethtaylor <taylor.kennethg@gmail.com>
2022-06-02 16:26:27 -0700

    feat: added FullHead ability

--- components/seo.js
+++ components/seo.js
@@ -1,10 +1,12 @@
 import Head from "next/head"
+import parse from "html-react-parser"
 
-export default function SEO({ title, description }) {
+export default function SEO({ title, description, fullhead }) {
     return (
         <Head>
-            <title>{ title }</title>
-            <meta name="description" content={description} />
+            {title && <title>{title}</title>}
+            {description && <meta name="description" content={description} />}
+            {fullhead && parse(fullhead) }
         </Head>
     )
 }
