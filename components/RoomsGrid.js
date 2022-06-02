f18e8f5dbdc62c77edba4c7786e70013adc40281
kennethtaylor <taylor.kennethg@gmail.com>
2022-06-02 16:25:57 -0700

    feat: added link to individual room

--- components/RoomsGrid.js
+++ components/RoomsGrid.js
@@ -6,7 +6,7 @@ import styled, { css } from 'styled-components';
 
 import styles from '../styles/roomsGrid.module.css'
 
-const filters = ['Outdoor Spaces', 'Workstation', 'Seating Area', 'Kitchenette', 'Ocean View', 'Bathtub', 'ADA Accessible'];
+const filterList = ['Outdoor Spaces', 'Workstation', 'Seating Area', 'Kitchenette', 'Ocean View', 'Bathtub', 'ADA Accessible'];
 
 const FilterContainer = styled.section`
     display: flex;    
@@ -21,8 +21,44 @@ const FilterContainer = styled.section`
 
 export default function RoomsGrid(props) {
 
+    const [filters, setFilters] = useState(filterList);
+    const [currentRooms, setCurrentRooms] = useState(null);
+
+    const newFilters = [];
+
     const { roomsgrid } = props
-    console.log(roomsgrid);
+
+    useEffect(() => { 
+        document.querySelectorAll('label.rooms-filter').forEach(label => { 
+            label.addEventListener('click', handleFilterClick);
+        })
+
+        setCurrentRooms(roomsgrid);
+    }, []);
+
+    const handleFilterClick = (e) => { 
+        e.preventDefault();
+
+        e.target.classList.toggle('active');
+
+        if (newFilters.includes(e.target.dataset.label_filter)) {
+            newFilters.pop(e.target.dataset.label_filter);
+        } else {
+            newFilters.push(e.target.dataset.label_filter);
+        }
+
+        filterRooms();
+    }
+
+    const filterRooms = () => {
+        console.log('new', newFilters);
+        const newRooms = [];
+
+        const filteredRooms = roomsgrid.filter(room => {
+            console.log(room.singleRooms.amenities)
+        });
+    }
+    console.log('roomsgrid', roomsgrid);
     return (
         <section className={styles.roomsGrid}>
             <FilterContainer>
@@ -30,25 +66,27 @@ export default function RoomsGrid(props) {
                     filters.map((filter, index) => { 
                         return (
                             <>
-                                <label className="rooms-filter"><span id="checkbox"></span><input type="checkbox" name="checkbox" value="value" />{filter}</label>
+                                <label className="rooms-filter sans-serif" data-label_filter={filter}><span id="checkbox"></span>{filter}</label>
                             </>
                         )
                     })
                 }
             </FilterContainer>
             <ul className={styles.gridList}>
-            {/* map over images */} 
             {
                     roomsgrid.map((room, index) => {
+                        const roomFilters = room.singleRooms.amenities
                         return (
-                            <li>
-                            <div key={room.title} className={styles.room} style={{ backgroundImage: `url(${room.featuredImage.node.mediaItemUrl})` }}>
-                                <Image className={styles.roomimage} src={room.featuredImage.node.mediaItemUrl} alt={room.altText} width={430} height={436} layout="intrinsic"/>
-                            </div>
-                            <div className={styles.text}>
-                                <p class="xs-heading uppercase black">Sleeps {room.singleRooms.sleeps} <span className={styles.keyFt}>{room.singleRooms.keyFeature}</span> <span className={styles.theme}>{room.singleRooms.theme}</span></p>
-                                <p className={`${styles.roommobile} serif heading black`}>{room.title}</p>
-                            </div>
+                            <li data-filter={roomFilters} >
+                                <a href={`/rooms/${room.slug}`}>
+                                    <div key={room.title} className={styles.room} style={{ backgroundImage: `url(${room.featuredImage.node.mediaItemUrl})` }}>
+                                        <Image className={styles.roomimage} src={room.featuredImage.node.mediaItemUrl} alt={room.altText} width={430} height={436} layout="intrinsic"/>
+                                    </div>
+                                    <div className={styles.text}>
+                                        <p class="xs-heading uppercase black">Sleeps {room.singleRooms.sleeps} <span className={styles.keyFt}>{room.singleRooms.keyFeature}</span> <span className={styles.theme}>{room.singleRooms.theme}</span></p>
+                                        <p className={`${styles.roommobile} serif heading black`}>{room.title}</p>
+                                    </div>
+                                </a>
                             </li>
                         )
                     })
