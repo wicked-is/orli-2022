03271d33062ba37f175db87b9d59dbbad4a23e1a
kennethtaylor <taylor.kennethg@gmail.com>
2022-06-02 13:36:44 -0700

    fix/feat: updated room query and styling

---
+++ pages/rooms/[room].js
@@ -0,0 +1,326 @@
+import { useEffect, useState } from 'react';
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import styles from '../../styles/singleRooms.module.css'
+
+import Hero from '../../components/hero';
+import SEO from '../../components/seo';
+
+import styled from 'styled-components';
+
+const SingleRoomContentContainer = styled.section`
+  display: flex;
+  fled-direction: row;
+  justify-content: space-around;
+  max-width: 80vw;
+  margin: auto; 
+  gap: 2rem;
+`
+
+const SingleRoomContent = styled.div`
+  width: 100%;
+  flex: 2;
+  margin-top: 3rem;
+`
+
+const SingleRoomBookingForm = styled.div`
+  width: 100%;
+  flex: 1;
+`
+
+const FeatureContainer = styled.div`
+  margin: 3rem 0;
+`
+
+const FeatureList = styled.div`
+  list-style-type: none;
+  display: inline-flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  gap: 1rem;
+
+  text-align: center;
+
+  img {
+    height: 100px;
+    width: auto;
+  }
+`
+
+const AmenitiesContainer = styled.div`
+  margin: 3rem 0; 
+`
+const AmenitiesList = styled.ul`
+  list-style-type: none;
+  columns: 2;
+
+  padding-inline-start: 0 !important;
+`
+
+const AboutOrliContainer = styled.div`
+  margin: 3rem 0;
+`
+
+const NeighborhoodContainer = styled.div`
+  margin: 3rem 0;
+`
+
+const GreyBackground = styled.div`
+  background-color: var(--lt-grey);
+  padding: 2rem;
+  position: relative;
+  top: -17.5rem;
+`
+
+const ReservationForm = styled.form``
+
+const ReservationFormLabel = styled.label`
+  display: flex;
+  justify-content: space-between; 
+  * {
+    flex: 1
+    max-width: 50%
+  }
+
+  &:first-of-type {
+    margin-top: 2rem;
+  }
+  &:last-of-type {
+    margin-bottom: 2rem;
+  }
+
+  input {
+    background-color: transparent;
+    border: 0px;
+    text-align: center;
+
+    &:after {
+
+    }
+  }
+`
+
+const ReservationButton = styled.button`
+  width: 100%;
+  height: 5rem;
+  color: #fff;
+  border: 0;
+  background-color: var(--brown);
+
+`
+
+export default function DefaultRoomsPage(props) {
+
+  const [checkInType, setCheckInType] = useState('text');
+  const [checkOutType, setCheckOutType] = useState('text');
+
+  const { room } = props.data.data;
+
+  useEffect(() => {
+      var tl =  gsap.timeline()
+          tl.fromTo('main', {opacity:0}, { opacity:1, delay: 0.5, duration: 1});
+
+      var sections = gsap.utils.toArray('.fade');
+
+      sections.forEach((section) => {
+          gsap.to(section, { autoAlpha: 1,
+              scrollTrigger: {
+                  trigger: section,
+                  start: "+=0 80%",
+                  scrub: false,
+                  markers: false,
+                  toggleActions: "play reverse play reverse"
+              }
+          });
+      })
+  }, [])
+  
+  return (
+    <>
+      <SEO fullhead={room.seo.fullHead} />
+      <Hero types="Single Room" imagePoster={room.featuredImage} />
+      <SingleRoomContentContainer>
+        <SingleRoomContent>
+          <div className="sans-serif body-copy black">
+            <p className="sans-serif-bold sub-heading">Sleeps {room.singleRooms.sleeps}</p>
+            <h1 className="heading">{room.title}</h1>
+            <p>{room.singleRooms.description}</p>
+            <p className="sans-serif xs-copy underline"><a>View Floorplan</a></p>
+          </div>
+
+          <FeatureContainer>
+            <p className="sans-serif-bold sub-heading">Features</p>
+            <FeatureList>
+              {
+                room.singleRooms.features.map((feature, index) => {
+                  return <div key={index} className="sans-serif body-copy black">
+                    <img src={feature.icon.mediaItemUrl} alt={feature.altText} />
+                    <p>{feature.label}</p>
+                  </div>
+                })
+              }
+            </FeatureList>
+          </FeatureContainer>
+
+          <AmenitiesContainer>
+            <p className="sans-serif-bold sub-heading">In-Room Amentities</p>
+            <AmenitiesList>
+              {
+                room.singleRooms.amenities.map((amenity, index) => {
+                  return <li key={index} style={{ marginBottom: '.5rem'}} className="sans-serif body-copy black">
+                    {amenity}
+                  </li>
+                })
+              }
+            </AmenitiesList>
+            <p className="sans-serif body-copy black" style={{ marginTop: '2rem' }}>When booking, see available add-on amenities including the Wellness Package, Pup Package, and more.</p>
+          </AmenitiesContainer>
+
+          <AboutOrliContainer>
+            <h2 className="heading">{room.singleRooms.aboutOrliTitle}</h2>
+            <div className="sans-serif body-copy black" dangerouslySetInnerHTML={{ __html: room.singleRooms.aboutOrliDescription }}></div>
+          </AboutOrliContainer>
+
+          <NeighborhoodContainer>
+            <h2 className="heading">{room.singleRooms.neighborhoodTitle}</h2>
+            <div className="sans-serif body-copy black" dangerouslySetInnerHTML={{ __html: room.singleRooms.neighborhoodDescription }}></div>
+            <ul style={{ paddingInline: 0 }}>
+              {
+                room.singleRooms.neighborhoodBullets.map((bullet, index) => {
+                  return <li key={index} className="serif black uppercase brown" style={{ display: 'flex', justifyContent: 'space-between', fontSize: '1.3rem' }}>
+                    <p>{bullet.pointOfInterest}</p>
+                    <p>{bullet.walkability}</p>
+                  </li>
+                })
+              }
+            </ul>
+          </NeighborhoodContainer>
+          <p className="sans-serif xs-copy underline arrow-left">Back to All Rooms</p>
+        </SingleRoomContent>
+        <SingleRoomBookingForm>
+          {/* form container component */}
+          <GreyBackground>
+            <p className="sans-serif-bold sub-heading">Sleeps {room.singleRooms.sleeps}</p>
+            <p className="heading">Reservations</p>
+            <ReservationForm>
+              <ReservationFormLabel className="sans-serif uppercase">Check In<input type={checkInType} onFocus={() => setCheckInType('date')} name="check-in" placeholder="Add Dates" className="sans-serif"/></ReservationFormLabel><br />
+              <ReservationFormLabel className="sans-serif uppercase">Check Out<input type={checkOutType} name="check-out" placeholder="Add Dates" onFocus={() => setCheckOutType('date')} className="sans-serif"/></ReservationFormLabel>
+              <ReservationButton className="sans-serif uppercase">Check Availability</ReservationButton>
+            </ReservationForm>
+          </GreyBackground>
+        </SingleRoomBookingForm>
+      </SingleRoomContentContainer>
+    </>
+  )
+}
+
+// Get all dynamic [room]s from the CMS
+export async function getStaticPaths() {
+
+    const res = await fetch(process.env.WP_GQL_API, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: `
+                query Rooms {
+                    rooms {
+                        nodes {
+                            slug
+                        }
+                    }
+                }
+            `
+        })
+    })
+
+    const rooms = await res.json()
+    
+    const paths = rooms.data.rooms.nodes.map(room => ({
+        params: { room: room.slug }
+    }));
+
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+// Get relative [slug] data
+export async function getStaticProps({ params }) {
+    const { room } = params
+    
+    // Query for Sections and SEO data
+    const roomsQuery = `
+      query AllComponents {
+        myOptionsPage {
+          options {
+            navigation {
+              navigationItems {
+                image {
+                  altText
+                  mediaItemUrl
+                }
+                label
+                link
+              }
+            }
+            announcementBar {
+              announcementBarText
+              isAnnouncementBarActive
+            }
+          }
+        }
+        room(id: "${room}", idType: SLUG) {
+          seo {
+            fullHead
+          }
+          singleRooms {
+            aboutOrliDescription
+            aboutOrliTitle
+            amenities
+            description
+            keyFeature
+            neighborhoodDescription
+            neighborhoodTitle
+            sleeps
+            squareFeet
+            theme
+            features {
+              label
+              icon {
+                altText
+                mediaItemUrl
+              }
+            }
+            neighborhoodBullets {
+              pointOfInterest
+              walkability
+            }
+          }
+          status
+          title
+          featuredImage {
+            node {
+              altText
+              mediaItemUrl
+            }
+          }
+        }
+      }`
+    
+    // Get page sections and SEO data
+    const res = await fetch(process.env.WP_GQL_API, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: roomsQuery })
+    })
+
+    const data = await res.json()
+    const page = data
+    
+    return {
+        props: {
+            data: page
+        }
+    }
+}
