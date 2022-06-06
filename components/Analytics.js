3bb28a3f22bcc4b7062e2f8a98c4404e8d3a9b53
kennethtaylor <taylor.kennethg@gmail.com>
2022-06-06 11:20:28 -0700

    feat: abstracting analytics

---
+++ components/Analytics.js
@@ -0,0 +1,27 @@
+import Script from "next/script"
+
+export default function Analytics(props) {
+
+    // const EXAMPLE_UA_CODE = "UA-130029095-1"
+    const UA_CODE = "UA-XXXXXXXXX-X"
+
+    return (
+        !props.siteNotLive && (
+            <>
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${UA_CODE}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', '${UA_CODE}');
+                    `}
+                </Script>
+            </>
+        )
+    )
+}
