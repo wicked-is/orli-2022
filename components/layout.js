26d2a90d467cd730ef4dca0e3085957fd3c880a3
kennethtaylor <taylor.kennethg@gmail.com>
2022-06-17 15:08:01 -0700

    edits

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Header from "../components/header";
 export default function Layout(props) {
     console.log("Layout props: ", props.topBar.isAnnouncementBarActive);
     return (
-        <div className={`${props.topBar.isAnnouncementBarActive !== true ? 'tob-bar-not-active': 'tob-bar-active'}`}>
+        <div className={`${props.topBar.isAnnouncementBarActive !== true ? 'tob-bar-not-active': 'top-bar-active'}`}>
             <Header navItems={props.navItems} topBar={props.topBar} />
             <main>
                 {props.children}
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 function MyApp(props) {
   const { Component, pageProps } = props
   const navItems = props.pageProps?.data?.data?.myOptionsPage.options.navigation.navigationItems || null;
-  const { announcementBar } = props.pageProps?.data?.data?.myOptionsPage.options || null;
+  const { announcementBar } = props.pageProps?.data?.data?.myOptionsPage?.options;
 
   return (
       <Layout navItems={navItems == null ? [] : navItems} topBar={announcementBar}>
