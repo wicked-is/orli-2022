37a585c26816f00085db8b218dd6105286c212b5
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-06-17 19:02:09 -0400

    Journal Update

--- components/AnchorBar.js
+++ components/AnchorBar.js
@@ -57,8 +57,9 @@ export default function AnchorBar(props) {
     useEffect(() => {  
 
         var maincontent = gsap.utils.toArray('main');
-          var journalnav = gsap.utils.toArray('.subnavjournal');
-      
+        var journalnav = gsap.utils.toArray('.top-bar-active .subnavjournal');
+        var journalnavtwo = gsap.utils.toArray('.tob-bar-not-active .subnavjournal');
+
             gsap.to(journalnav, {
               scrollTrigger: {
                   trigger: journalnav,
@@ -68,10 +69,21 @@ export default function AnchorBar(props) {
                   pin: journalnav,
                   markers: false,
                   pinSpacing: false,
-                  scrub: true,
-                  onRefresh: self => self.pin.parentNode.style.float = "right",
-              }, y: 0
-            });
+                  scrub: true
+              }, y: 0});
+          
+            gsap.to(journalnavtwo, {
+                 scrollTrigger: {
+                      trigger: journalnavtwo,
+                      start: "top-=0 88px",
+                      endTrigger: maincontent,
+                      end: "bottom-=0 bottom-=150",
+                      pin: journalnavtwo,
+                      markers: false,
+                      pinSpacing: false,
+                      scrub: true
+                  }, y: 0});
+
         // var tl =  gsap.timeline()
         // tl.fromTo('header', {opacity: 0}, { opacity:1, duration: 0.5});
         // tl.to('main', { opacity:1, duration: 0.6});
@@ -95,7 +107,7 @@ export default function AnchorBar(props) {
         //         }, y: 0
         //     });
         // }
-    }, [])
+    },[])
 
     return (
         <SubNavContainer className="subnavjournal">              
