7ecf052df09317ed6f7161d833fc1ed6e5923ec2
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-06-27 20:33:25 -0400

    Background gradient added to RoomsSlider

--- components/roomSlider.js
+++ components/roomSlider.js
@@ -110,6 +110,7 @@ export default function RoomSlider(props) {
                     })
                 }
             </div>
+            <div class={styles.bottomgradient}></div>
         </section>
     )
 }
