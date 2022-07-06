bf439aa5d2d9057562a6ddca916d20a9facae4ed
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-07-06 15:02:38 -0400

    Orli animation

--- components/ourMission.js
+++ components/ourMission.js
@@ -16,11 +16,11 @@ export default function OurMission(props) {
         var timeline = gsap.timeline({ repeat: -1 });
         
         timeline.fromTo(".cursor", {autoAlpha: 0, x:0},{autoAlpha: 1, duration: 0.5, repeat: -1, ease: "steps(1)"})
-            .to(".textAnimation", {text: {value: "Intentional"}, duration: 1, delay: 1, ease: "none"})
+            .to(".textAnimation", {text: {value: "intentional"}, duration: 1, delay: 1, ease: "none"})
             .to(".textAnimation", {delay: 2, duration: 0.5, autoAlpha: 0, display: 'none'})
-            .to(".textAnimation2", {text: {value: "Local-Minded"}, duration: 1, delay: 1, ease: "none"})
+            .to(".textAnimation2", {text: {value: "local-minded"}, duration: 1, delay: 1, ease: "none"})
             .to(".textAnimation2", {delay: 2, duration: 0.5, autoAlpha: 0, display: 'none'})
-            .to(".textAnimation3", {text: {value: "Effortless"}, duration: 1.5, delay: 1, ease: "none"})
+            .to(".textAnimation3", {text: {value: "effortless"}, duration: 1.5, delay: 1, ease: "none"})
             .to(".textAnimation3", {delay: 3, duration: 0.5, autoAlpha: 0, display: 'none'})
             .add(() => timeline.restart())
     },[])
