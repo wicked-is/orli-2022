cea2daf2e350625f9c1f125b0f27cc5d09d02567
kennethtaylor <taylor.kennethg@gmail.com>
2022-07-26 11:09:05 -0700

    Update build error

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp(props) {
   console.log(props);
   const { Component, pageProps } = props
   const navItems = props.pageProps?.data?.data?.myOptionsPage.options.navigation.navigationItems || null;
-  const { announcementBar } = props.pageProps?.data?.data?.myOptionsPage?.options;
+  const announcementBar = props.pageProps?.data?.data?.myOptionsPage?.options?.announcementBar;
 
   return (
       <Layout navItems={navItems == null ? [] : navItems} topBar={announcementBar}>
