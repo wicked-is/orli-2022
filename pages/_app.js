fd3a26ffe33d5a5b1887b005b36092390eeda270
kennethtaylor <taylor.kennethg@gmail.com>
2022-08-03 16:01:53 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,6 @@ function MyApp(props) {
   const announcementBar = props.pageProps?.data?.data?.myOptionsPage?.options?.announcementBar;
   const socialItems = props.pageProps?.data?.data?.myOptionsPage?.options?.socialFooter;
 
-  console.log(props);
-
   return (
       <Layout navItems={navItems == null ? [] : navItems} topBar={announcementBar} footerImages={socialItems}>
         <Component {...pageProps} />
