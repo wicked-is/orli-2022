9b9ca0ee2c7fd117cd03bb06bc849f59324d2d61
kennethtaylor <taylor.kennethg@gmail.com>
2022-08-18 14:57:57 -0700

    Update ExploreMorePosts.js

--- components/ExploreMorePosts.js
+++ components/ExploreMorePosts.js
@@ -50,7 +50,7 @@ export default function ExploreMorePosts(props) {
                         posts.map(post => {
                             return (
                                 <ExploreMorePostsTile key={post.title}>
-                                    <Link href={post.uri}><Image src={post.featuredImage.node.mediaItemUrl} alt={post.featuredImage.node.altText} width="430" height="436" layout="responsive" /></Link>
+                                    <Link href={post.uri}><Image src={post.featuredImage.node.mediaItemUrl} alt={post.featuredImage.node.altText} width="500" height="436" layout="responsive" style={{ objectFit: 'cover' }} /></Link>
                                     <p className="xs-heading uppercase black">{post.categories.nodes[0].name}</p>
                                     <Link href={post.uri}><p className="heading left">{post.title}</p>
                                     </Link>
