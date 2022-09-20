5c7a1465827178b71eb0ff1d1fd6752a3515fdde
kennethtaylor <taylor.kennethg@gmail.com>
2022-09-20 00:47:25 -0700

    Update seo.js

--- components/seo.js
+++ components/seo.js
@@ -6,6 +6,7 @@ export default function SEO(props) {
     const { title, description, fullhead } = props;
     return (
         <Head>
+            <meta name="robots" content="noindex" />
             {title && !fullhead && <title>{title}</title>}
             {description && !fullhead && <meta name="description" content={description} />}
             {fullhead && parse(fullhead) }
