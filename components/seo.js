aa65fb288a6b30ba24e9ca90abc620d266dd888a
kennethtaylor <taylor.kennethg@gmail.com>
2022-09-20 00:46:25 -0700

    Pushed live // media coverage

--- components/seo.js
+++ components/seo.js
@@ -6,7 +6,6 @@ export default function SEO(props) {
     const { title, description, fullhead } = props;
     return (
         <Head>
-            <meta name="robots" content="noindex" />
             {title && !fullhead && <title>{title}</title>}
             {description && !fullhead && <meta name="description" content={description} />}
             {fullhead && parse(fullhead) }
