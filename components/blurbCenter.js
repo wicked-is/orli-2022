4c866804572cbffbc336b3c2a72187229b0caedf
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-09-22 15:53:55 +0200

    Gsap Loading Blank Fix
    Fixed following components, blurbLeft.js, bigimageSmallcontent.js and blurbCenter.js

--- components/blurbCenter.js
+++ components/blurbCenter.js
@@ -1,7 +1,27 @@
+import { useEffect } from 'react';
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import styles from '../styles/blurbCenter.module.css';
+gsap.registerPlugin(ScrollTrigger);
 
 export default function BlurbCenter(props) {
-    
+    useEffect(() => {
+        var blurbcentersections = gsap.utils.toArray('.blurbcenterfade');
+
+        blurbcentersections.forEach((blurbcentersection) => {
+            gsap.to(blurbcentersection, { autoAlpha: 1,
+                scrollTrigger: {
+                    trigger: blurbcentersection,
+                    start: "+=0 80%",
+                    scrub: false,
+                    markers: false,
+                    toggleActions: "play reverse play reverse"
+                }
+            });
+        });
+
+    },[])
+
     const {
         blurb,
         greyBackground,
@@ -22,13 +42,13 @@ export default function BlurbCenter(props) {
                 </video>
             ) }
             <div className="max-60 center">
-                { !mp4ExternalLink && <div className={`${styles.icon} fadein`}>
+                { !mp4ExternalLink && <div className={`${styles.icon} blurbcenterfade`}>
                     <img src={icon?.mediaItemUrl} alt={icon?.altText}/>
                 </div> }
                 {headline && ( 
-                    <p className="serif heading black center fadein">{headline}</p>
+                    <p className="serif heading black center blurbcenterfade">{headline}</p>
                 )}
-                <p className="heading-italic fadein">
+                <p className="heading-italic blurbcenterfade">
                     { blurb }
                 </p>
             </div>
