b105435c64bd612606701d1199cf32d0b00b1484
kennethtaylor <taylor.kennethg@gmail.com>
2022-09-23 12:38:06 -0700

    Removing email subscribe redirect

--- components/footer.js
+++ components/footer.js
@@ -54,6 +54,7 @@ export default function Footer(props) {
     const size = useWindowSize();
     
     const [email, setEmail] = useState('');
+    const [success, setSuccess] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -69,7 +70,8 @@ export default function Footer(props) {
     }
 
     const handleResponse = async (res) => {
-        window.location.href = "/thank-you/";
+        if (res.state)
+            setSuccess(true);
     }
 
     return (
@@ -79,17 +81,19 @@ export default function Footer(props) {
                 <>
                     <MobileFooterContainer className="mobile-footer">
                     <div className="email-signup">
-                                <p className="serif white left">Exclusive Offers, News & Events</p>
-                                <form id="emailcapture" onSubmit={handleSubmit}>
-                                        <input id="email" 
-                                        name="email" 
-                                        value={email} 
-                                        placeholder="Enter Email*"
-                                        onChange={() => setEmail(event.target.value)}
-                                        />
-                                 <button className="submit">Submit</button>
-                                </form>
-                        </div>
+                        <p className="serif white left">Exclusive Offers, News & Events</p>
+                        { success ? <div className="sans-serif body-copy white left">
+                            <p>Success. Thanks for joining us.</p>
+                        </div> : <form id="emailcapture" onSubmit={handleSubmit}>
+                            <input id="email" 
+                                name="email" 
+                                value={email} 
+                                placeholder="Enter Email*"
+                                onChange={() => setEmail(event.target.value)}
+                            />
+                            <button className="submit">Submit</button>
+                        </form>}
+                    </div>
                         <Link href="/">
                             <a>
                                 <img
@@ -161,106 +165,110 @@ export default function Footer(props) {
                         </ColumnContainer>
                     </MobileFooterContainer>
                 </>
-            ) : ( <footer className="footer desk-footer">
-                <div className="inner-container">
-                    <div>
+            ) : ( 
+                <footer className="footer desk-footer">
+                    <div className="inner-container">
                         <div>
-                        <Link href="/">
-                            <a>
-                        <img
-                            className="footer-logo" 
-                            src="https://orlidev.wpengine.com/wp-content/uploads/2022/01/logo-orli.svg" 
-                            width={380} height={75} 
-                            layout="responsive" />
-                            </a>
-                        </Link>
-                        </div>
-                        <div>
-                            <address className="sans-serif xs-copy white left">
-                                Orli La Jolla<br/>7753 Draper Ave,<br/>La Jolla, CA 92037
-                            </address>
-                                
-                            <p className="directions xs-copy white left">
-                                <Link href="https://www.google.com/search?q=7753+Draper+Ave%2C+La+Jolla%2C+CA+92037&rlz=1C5CHFA_enUS963US963&oq=7753+Draper+Ave%2C+La+Jolla%2C+CA+92037&aqs=chrome..69i57j0i512j69i60l3.816j0j4&sourceid=chrome&ie=UTF-8#">Get Directions</Link>
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                    <div>
-                        <div className="email-signup">
-                                <p className="serif white left">Exclusive Offers, News & Events</p>
-                                <form id="emailcapture" action="https://hooks.zapier.com/hooks/catch/2001353/bc24kj3/"  onSubmit={handleSubmit}>
-                                    <input id="email" 
-                                        name="email" 
-                                        value={email} 
-                                        placeholder="Enter Email*"
-                                        onChange={() => setEmail(event.target.value)}
-                                        />
-                                    <button className="submit">Submit</button>
-                                </form>
-                        </div>
-                    </div>
-                    
-                        <div>
-                            <div className="footer-links">
-                                <Link href="/find-your-room">Rooms</Link>
-                                <Link href="/our-story">Story</Link>
-                                <Link href="/amenities">Amenities</Link>
-                                <Link href="/the-journal">Journal</Link>
-                                <Link href="/discoveries">Discoveries</Link>
-                                <Link href="/gallery">Gallery</Link>
-                                <Link href="/gatherings">Events</Link>
+                            <div>
+                            <Link href="/">
+                                <a>
+                            <img
+                                className="footer-logo" 
+                                src="https://orlidev.wpengine.com/wp-content/uploads/2022/01/logo-orli.svg" 
+                                width={380} height={75} 
+                                layout="responsive" />
+                                </a>
+                            </Link>
+                            </div>
+                            <div>
+                                <address className="sans-serif xs-copy white left">
+                                    Orli La Jolla<br/>7753 Draper Ave,<br/>La Jolla, CA 92037
+                                </address>
+                                    
+                                <p className="directions xs-copy white left">
+                                    <Link href="https://www.google.com/search?q=7753+Draper+Ave%2C+La+Jolla%2C+CA+92037&rlz=1C5CHFA_enUS963US963&oq=7753+Draper+Ave%2C+La+Jolla%2C+CA+92037&aqs=chrome..69i57j0i512j69i60l3.816j0j4&sourceid=chrome&ie=UTF-8#">Get Directions</Link>
+                                </p>
                             </div>
                         </div>
                         <div>
+                        <div>
+                            <div className="email-signup">
+                                    <p className="serif white left">Exclusive Offers, News & Events</p>
+                                    { success ? <div style={{ minHeight: '47px', display: 'flex', alignContent: 'center' }}>
+                                            <p className="sans-serif copyright xs-copy white left" style={{ verticalAlign: 'center'}}>Success. Thanks for joining us.</p>
+                                        </div> : <form id="emailcapture" action="https://hooks.zapier.com/hooks/catch/2001353/bc24kj3/"  onSubmit={handleSubmit}>
+                                        <input id="email" 
+                                            name="email" 
+                                            value={email} 
+                                            placeholder="Enter Email*"
+                                            onChange={() => setEmail(event.target.value)}
+                                            />
+                                        <button className="submit">Submit</button>
+                                    </form> }
+                            </div>
+                        </div>
+                        
+                            <div>
+                                <div className="footer-links">
+                                    <Link href="/find-your-room">Rooms</Link>
+                                    <Link href="/our-story">Story</Link>
+                                    <Link href="/amenities">Amenities</Link>
+                                    <Link href="/the-journal">Journal</Link>
+                                    <Link href="/discoveries">Discoveries</Link>
+                                    <Link href="/gallery">Gallery</Link>
+                                    <Link href="/gatherings">Events</Link>
+                                </div>
+                            </div>
                             <div>
                                 <div>
-                                    <p className="sans-serif xs-copy white left mt-0" id="phone-number">T: <a href="tel:6195760806">619.576.0806</a><br/>
-                                    E: <a href="mailto:hello@stayorli.com">hello@stayorli.com</a></p>
-                                    <ul className="socials">
-                                        <li>
-                                            <a href="https://www.instagram.com/stayorli/" target="_blank" rel="noopener noreferrer">
-                                                <Image
-                                                    src="https://orlidev.wpengine.com/wp-content/uploads/2022/03/instagram-copy.svg"
-                                                    alt="Instagram Logo"
-                                                    className="instagram"
-                                                    width={30}
-                                                    height={30} />
-                                            </a>
-                                        </li>
-                                        {/* <li>
-                                            <Link href="/">
-                                                <Image
-                                                    src="https://orlidev.wpengine.com/wp-content/uploads/2022/03/facebook.svg"
-                                                    alt="facebook Logo"
-                                                    className="facebook"
-                                                    width={14}
-                                                    height={30} />
-                                            </Link>
-                                        </li> */} 
-                                        <li>
-                                            <a target="_blank" href="https://open.spotify.com/playlist/5dt0gGfjFDjfd4CJe22E8T?si=278d0b0810534a75" rel="noopener noreferrer">
-                                                <Image
-                                                    src="https://orlidev.wpengine.com/wp-content/uploads/2022/08/spotify-brands.svg"
-                                                    alt="spotify logo"
-                                                    className="spotify"
-                                                    width={30}
-                                                    height={30} />
-                                            </a>
-                                        </li>
-                                    </ul>
+                                    <div>
+                                        <p className="sans-serif xs-copy white left mt-0" id="phone-number">T: <a href="tel:6195760806">619.576.0806</a><br/>
+                                        E: <a href="mailto:hello@stayorli.com">hello@stayorli.com</a></p>
+                                        <ul className="socials">
+                                            <li>
+                                                <a href="https://www.instagram.com/stayorli/" target="_blank" rel="noopener noreferrer">
+                                                    <Image
+                                                        src="https://orlidev.wpengine.com/wp-content/uploads/2022/03/instagram-copy.svg"
+                                                        alt="Instagram Logo"
+                                                        className="instagram"
+                                                        width={30}
+                                                        height={30} />
+                                                </a>
+                                            </li>
+                                            {/* <li>
+                                                <Link href="/">
+                                                    <Image
+                                                        src="https://orlidev.wpengine.com/wp-content/uploads/2022/03/facebook.svg"
+                                                        alt="facebook Logo"
+                                                        className="facebook"
+                                                        width={14}
+                                                        height={30} />
+                                                </Link>
+                                            </li> */} 
+                                            <li>
+                                                <a target="_blank" href="https://open.spotify.com/playlist/5dt0gGfjFDjfd4CJe22E8T?si=278d0b0810534a75" rel="noopener noreferrer">
+                                                    <Image
+                                                        src="https://orlidev.wpengine.com/wp-content/uploads/2022/08/spotify-brands.svg"
+                                                        alt="spotify logo"
+                                                        className="spotify"
+                                                        width={30}
+                                                        height={30} />
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div>
+                                    <p className="sans-serif copyright xs-copy white left">© 2022 Orli La Jolla | <Link href="/privacy-policy">Privacy Policy</Link> | <Link href="/cookie-policy">Cookie Policy</Link> | <Link href="/terms-conditions">Terms & Conditions</Link></p>
                                 </div>
                             </div>
                         </div>
-
-                        <div>
-                            <div>
-                                <p className="sans-serif copyright xs-copy white left">© 2022 Orli La Jolla | <Link href="/privacy-policy">Privacy Policy</Link> | <Link href="/cookie-policy">Cookie Policy</Link> | <Link href="/terms-conditions">Terms & Conditions</Link></p>
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </footer> ) }
+                </footer> 
+            ) }
         </>
     )
 }
