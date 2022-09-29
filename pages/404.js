9df6c767a9b67a101f3c454b04578ec1780ffb72
kennethtaylor <taylor.kennethg@gmail.com>
2022-09-29 15:54:41 -0700

    Updating 404 styling

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,8 @@
 import Link from "next/link"
+import { useEffect } from "react"
 import styled from "styled-components"
 import Header from "../components/header"
+import { gsap } from "gsap/dist/gsap";
 
 const FullSection = styled.section`
     display: flex;
@@ -11,21 +13,44 @@ const FullSection = styled.section`
     padding-top: 4rem;
 `
 const Title = styled.p`
-    font-weight: 400;
-    font-style: normal;
     color: ${props => props.white ? '#fff' : '#000'};
-    font-size: var(--headline);
-    margin-top: 0;
-    margin-bottom: ${props => props.mb ? '3rem' : '.5rem'};
     z-index: 1;
+    font-size: var(--heading);
+    font-family: essonnes-display, serif;
+    font-weight: 300;
+    line-height: 1.2;
+    font-style: normal;
+    margin-block-start: 0em;
+    margin-block-end: 2rem;
 `
 const StyledLink = styled.a`
-    color: white;
+    color: var(--primary);
     text-transform: uppercase;
     text-decoration: underline;
 `
 
 export default function PageNotFound(props) {
+
+    useEffect(() => {
+        var tl = gsap.timeline()
+            tl.fromTo('header', {opacity: 0}, { opacity:1, duration: 0.5});
+            tl.to('main', { opacity:1, duration: 0.6});
+        tl.play();
+        // var sections = gsap.utils.toArray('.fadein');
+
+        // sections.forEach((section) => {
+        //     gsap.to(section, { autoAlpha: 1,
+        //         scrollTrigger: {
+        //             trigger: section,
+        //             start: "+=0 0%",
+        //             scrub: false,
+        //             markers: false,
+        //             toggleActions: "play reverse play reverse"
+        //         }
+        //     });
+        // });
+
+    },[])
     
     return (
         <FullSection>
