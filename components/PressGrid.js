0336a692852a06d41a8cf7c3c06f3d2b92a4677f
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-10-03 20:26:05 +0200

    Added Press to Footer

--- components/PressGrid.js
+++ components/PressGrid.js
@@ -1,43 +1,66 @@
-import styled, { css } from 'styled-components';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect } from 'react';
+import styled, { css } from 'styled-components';
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const PressGridContainer = styled.section`
     display: flex;
-    align-items; flex-start;
+    align-items: flex-start;
     flex-wrap: wrap;
     justify-content: center;
     margin-bottom: 6rem;
 `
+
 const PressTile = styled.div`
     display: inline;
     width: 27.33%;
-    margin: 0.5rem 0.5rem 1.5rem 0.5rem;
+    margin: 0.5rem 0.5rem 0.5rem 0.5rem;
     position: relative;
 
     & img.blogImage {
         cursor: pointer;
     }
+
     & img.blogImage:hover p.heading {
         color: var(--brown) !important;
     }
+
     & p.heading:hover {
         color: var(--brown);
     }
+
+    ${props => props.featured && css`
+        display: block;
+        min-height: 70vh;
+        width: 100% !important;
+        background-image: url(${props.background}) !important;
+        background-size: cover !important;
+        -webkit-background-size: cover !important;
+        -moz-background-size: cover !important;
+        -o-background-size: cover !important;
+        background-position: center !important;
+        background-repeat: no-repeat !important;
+        margin: 8rem 0 !important;
+        padding: 4rem 6rem;
+        cursor: pointer;
+
+        img, span { display: none !important; }
+        p { color: #fff; }
+        & p.heading:hover {color: #fff !important;}
+    `}
     @media only screen and (max-width:600px){
-        & {width: 98%;
-        margin: 0.5rem 0.5rem 1rem 0.5rem;
-    }
+        & {width: 98%;}
     }
 `
+
 const PubTitle = styled.p`
     cursor: pointer;
     transition: 0.3s ease all;
 `
+
 const PressTitle = styled.p`
     cursor: pointer;
     transition: 0.3s ease all;
@@ -61,33 +84,25 @@ export default function PressGrid(props) {
                 }
             });
         });
-    },[])
 
+    },[])
     return (
         <PressGridContainer>
             {
-                posts && posts.map((post, index) => {     
-                    const featured = post.blogPost.featured === null ?
-                        false :
-                        post.blogPost.featured;
-                    
-                    const category = post.categories.nodes[0].name;
+                posts && posts.map((post, index) => {
 
                     return (
-                            <PressTile key={index} featured={featured} className="fadein">
+                            <PressTile key={index} className="fadein">
                                 <a target="_blank" href={post.singlePress.externalLink} rel="noreferrer noopener">
                                     <Image src={post.featuredImage.node.mediaItemUrl} width={500} height={400} alt={post.featuredImage.node.altText} layout="intrinsic" className="blogImage" />
                                 </a>
                                 <a target="_blank" href={post.singlePress.externalLink} rel="noreferrer noopener">
-                                    { featured && ( 
-                                        <PubTitle className="sans-serif xs-heading black uppercase">{post.singlePress.publicationName}</PubTitle>
-                                     ) }
-                                    { !featured && ( <PubTitle className="sans-serif xs-heading black uppercase">{post.singlePress.publicationName}</PubTitle> ) }
+                                    <PubTitle className="sans-serif xs-heading black uppercase">{post.singlePress.publicationName}</PubTitle>
                                     <PressTitle className="serif heading black">{post.title}</PressTitle>
                                 </a>
                             </PressTile>
-                            )
-                    })
+                    )
+                })
             }
         </PressGridContainer>
     )
