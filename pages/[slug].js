5d3d3349f606ff1cf29f46c54b8904342baf1c3d
kennethtaylor <taylor.kennethg@gmail.com>
2022-10-03 10:10:44 -0700

    Updating Slug

--- pages/[slug].js
+++ pages/[slug].js
@@ -274,7 +274,7 @@ export async function getStaticProps({ params }) {
             name
           }
         }
-        page(id: "${slug}", idType: URI) {
+        page(id: "${slug}", idType: URI, first: 999) {
           seo {
             title
             metaDesc
@@ -910,7 +910,7 @@ export async function getStaticProps({ params }) {
             }
           }
         }
-        post(id: "${slug}", idType: SLUG) {
+        post(id: "${slug}", idType: SLUG, first: 999) {
           seo {
             title
             metaDesc
