f8efc7add2e9601bb7c819ee84104b7ed432208d
kcreinawicked <61247292+kcreinawicked@users.noreply.github.com>
2022-10-05 22:13:50 +0200

    Revert "Press Page Edits"
    This reverts commit 0fb1ea44511d5e1caa95aeea8ed6f395107ce5ac.

--- components/PressGrid.js
+++ components/PressGrid.js
@@ -48,7 +48,7 @@ const PressTile = styled.div`
         -webkit-background-size: cover !important;
         -moz-background-size: cover !important;
         -o-background-size: cover !important;
-        background-position: center center;
+        background-position: center !important;
         background-repeat: no-repeat !important;
         margin: 8rem 0 !important;
         padding: 4rem 6rem;
@@ -84,7 +84,7 @@ export default function PressGrid(props) {
     const { posts } = props;
 
     useEffect(() => {
-        var sections = gsap.utils.toArray('.fadeinpress');
+        var sections = gsap.utils.toArray('.fadein');
 
         sections.forEach((section) => {
             gsap.to(section, { autoAlpha: 1,
@@ -105,7 +105,7 @@ export default function PressGrid(props) {
                 posts && posts.map((post, index) => {
 
                     return (
-                            <PressTile key={`${index}${post.slug}`} className="fadeinpress">
+                            <PressTile key={`${index}${post.slug}`} className="fadein">
                                 <a target="_blank" href={post.singlePress.externalLink} rel="noreferrer noopener">
                                     <Image src={post.featuredImage.node.mediaItemUrl} width={500} height={400} alt={post.featuredImage.node.altText} layout="responsive" className="blogImage" />
                                 </a>
