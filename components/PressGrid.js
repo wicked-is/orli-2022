c6191493b0fd4bdb097cca38eb5287652b2bfc0b
kennethtaylor <taylor.kennethg@gmail.com>
2022-10-05 16:22:30 -0700

    Updating arbitrary edit

--- components/PressGrid.js
+++ components/PressGrid.js
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect } from 'react';
-import styled, { css } from 'styled-components';
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import styled, { css } from "styled-components";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
@@ -13,13 +13,13 @@ const PressGridContainer = styled.section`
     flex-wrap: wrap;
     justify-content: left;
     margin: auto 6rem 6rem 6rem;
-    @media only screen and (max-width:831px) {
+    @media only screen and (max-width: 831px) {
         & {
             width: calc(100% / 4rem);
             margin: auto 4rem 6rem 4rem;
         }
     }
-`
+`;
 
 const PressTile = styled.div`
     display: inline;
@@ -39,84 +39,116 @@ const PressTile = styled.div`
         color: var(--brown);
     }
 
-    ${props => props.featured && css`
-        display: block;
-        min-height: 70vh;
-        width: 100% !important;
-        background-image: url(${props.background}) !important;
-        background-size: cover !important;
-        -webkit-background-size: cover !important;
-        -moz-background-size: cover !important;
-        -o-background-size: cover !important;
-        background-position: center center;
-        background-repeat: no-repeat !important;
-        margin: 8rem 0 !important;
-        padding: 4rem 6rem;
-        cursor: pointer;
+    ${props =>
+        props.featured &&
+        css`
+            display: block;
+            min-height: 70vh;
+            width: 100% !important;
+            background-image: url(${props.background}) !important;
+            background-size: cover !important;
+            -webkit-background-size: cover !important;
+            -moz-background-size: cover !important;
+            -o-background-size: cover !important;
+            background-position: center center;
+            background-repeat: no-repeat !important;
+            margin: 8rem 0 !important;
+            padding: 4rem 6rem;
+            cursor: pointer;
 
-        img, span { display: none !important; }
-        p { color: #fff; }
-        & p.heading:hover {color: #fff !important;}
-    `}
-    @media only screen and (max-width:1257px){
-        & {width: 29.33%;}
+            img,
+            span {
+                display: none !important;
+            }
+            p {
+                color: #fff;
+            }
+            & p.heading:hover {
+                color: #fff !important;
+            }
+        `}
+    @media only screen and (max-width:1257px) {
+        & {
+            width: 29.33%;
+        }
     }
-    @media only screen and (max-width:991px){
-        & {width: 45%;}
+    @media only screen and (max-width: 991px) {
+        & {
+            width: 45%;
+        }
     }
-    @media only screen and (max-width:767px){
-        & {width: 100%; margin: 0 0 3rem 0;}
+    @media only screen and (max-width: 767px) {
+        & {
+            width: 100%;
+            margin: 0 0 3rem 0;
+        }
     }
-`
+`;
 
 const PubTitle = styled.p`
     cursor: pointer;
     transition: 0.3s ease all;
-`
+`;
 
 const PressTitle = styled.p`
     cursor: pointer;
-    transition: 0.30s ease all;
-`
+    transition: 0.3s ease all;
+`;
 
 export default function PressGrid(props) {
-
     const { posts } = props;
 
     useEffect(() => {
-        var sections = gsap.utils.toArray('.fadeinpress');
+        var sections = gsap.utils.toArray(".fadeinpress");
 
-        sections.forEach((section) => {
-            gsap.to(section, { autoAlpha: 1,
+        sections.forEach(section => {
+            gsap.to(section, {
+                autoAlpha: 1,
                 scrollTrigger: {
                     trigger: section,
                     start: "+=0 80%",
                     scrub: false,
                     markers: false,
-                    toggleActions: "play reverse play reverse"
-                }
+                    toggleActions: "play reverse play reverse",
+                },
             });
         });
-
-    },[])
+    }, []);
     return (
         <PressGridContainer>
-            {
-                posts && posts.map((post, index) => {
-
+            {posts &&
+                posts.map((post, index) => {
                     return (
-                            <PressTile key={`${index}${post.slug}`} className="fadeinpress">
-                                <a target="_blank" href={post.singlePress.externalLink} rel="noreferrer noopener">
-                                    <Image src={post.featuredImage.node.mediaItemUrl} width={500} height={400} alt={post.featuredImage.node.altText} layout="responsive" className="blogImage" />
-                                </a>
-                                <a target="_blank" href={post.singlePress.externalLink} rel="noreferrer noopener">
-                                    <PubTitle className="sans-serif xs-heading black uppercase">{post.singlePress.publicationName}</PubTitle>
-                                    <PressTitle className="serif heading black">{post.title}</PressTitle>
-                                </a>
-                            </PressTile>
-                    )
-                })
-            }
+                        <PressTile
+                            key={`${index}${post.slug}`}
+                            className="fadeinpress">
+                            <a
+                                target="_blank"
+                                href={post.singlePress.externalLink}
+                                rel="noreferrer noopener">
+                                <Image
+                                    src={post.featuredImage.node.mediaItemUrl}
+                                    width={500}
+                                    height={400}
+                                    alt={post.featuredImage.node.altText}
+                                    layout="responsive"
+                                    className="blogImage"
+                                />
+                            </a>
+                            <a
+                                target="_blank"
+                                href={post.singlePress.externalLink}
+                                rel="noreferrer noopener">
+                                <PubTitle className="sans-serif xs-heading black uppercase">
+                                    {post.singlePress.publicationName}
+                                </PubTitle>
+                                <PressTitle className="serif heading black">
+                                    {post.title}
+                                </PressTitle>
+                            </a>
+                        </PressTile>
+                    );
+                })}
         </PressGridContainer>
-    )
+    );
 }
