92f96301690c7d2b6e669d55eadab13626f78ebb
kennethtaylor <taylor.kennethg@gmail.com>
2022-10-06 14:16:25 -0700

    Adding press styles

--- components/BlogGrid.js
+++ components/BlogGrid.js
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect } from 'react';
-import styled, { css } from 'styled-components';
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import styled, { css } from "styled-components";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
@@ -12,12 +12,12 @@ const BlogGridContainer = styled.section`
     flex-wrap: wrap;
     justify-content: center;
     margin-bottom: 6rem;
-`
+`;
 
 const BlogTitle = styled.p`
     cursor: pointer;
     transition: 0.3s ease all;
-`
+`;
 
 const BlogTile = styled.div`
     display: inline;
@@ -60,7 +60,7 @@ const BlogTile = styled.div`
         position: relative;
     }
 
-    @media only screen and (max-width:1074px) {
+    @media only screen and (max-width: 1074px) {
         &:first-child,
         &:nth-child(1n + 3) {
             width: 26.33%;
@@ -80,7 +80,7 @@ const BlogTile = styled.div`
         }
     }
 
-    @media only screen and (max-width:913px) {
+    @media only screen and (max-width: 913px) {
         &:first-child,
         &:nth-child(1n + 3) {
             width: 43%;
@@ -97,14 +97,14 @@ const BlogTile = styled.div`
             margin: 1rem;
         }
     }
-    @media only screen and (max-width:768px) {
+    @media only screen and (max-width: 768px) {
         &:nth-child(3n + 4) {
             width: 90%;
             margin: 1rem;
-            padding: 4rem calc(5% + .5rem);
+            padding: 4rem calc(5% + 0.5rem);
         }
     }
-    @media only screen and (max-width:414px) {
+    @media only screen and (max-width: 414px) {
         &:first-child,
         &:nth-child(1n + 3) {
             width: 98%;
@@ -122,74 +122,105 @@ const BlogTile = styled.div`
         }
     }
 
-    ${props => props.featured && css`
-        display: block;
-        min-height: 70vh;
-        width: 100% !important;
-        background-image: url(${props.background}) !important;
-        background-size: cover !important;
-        -webkit-background-size: cover !important;
-        -moz-background-size: cover !important;
-        -o-background-size: cover !important;
-        background-position: center !important;
-        background-repeat: no-repeat !important;
-        margin: 8rem 0 !important;
-        padding: 4rem 6rem;
-        cursor: pointer;
+    ${props =>
+        props.featured &&
+        css`
+            display: block;
+            min-height: 70vh;
+            width: 100% !important;
+            background-image: url(${props.background}) !important;
+            background-size: cover !important;
+            -webkit-background-size: cover !important;
+            -moz-background-size: cover !important;
+            -o-background-size: cover !important;
+            background-position: center !important;
+            background-repeat: no-repeat !important;
+            margin: 8rem 0 !important;
+            padding: 4rem 6rem;
+            cursor: pointer;
 
-        img, span { display: none !important; }
+            img,
+            span {
+                display: none !important;
+            }
 
-        p { color: #fff; }
+            p {
+                color: #fff;
+            }
 
-        & p.heading:hover {color: #fff !important;}
-    `}
-`
+            & p.heading:hover {
+                color: #fff !important;
+            }
+        `}
+`;
 
 export default function BlogGrid(props) {
-
     const { posts } = props;
 
     useEffect(() => {
-        var sections = gsap.utils.toArray('.fadein');
+        var sections = gsap.utils.toArray(".fadein");
 
-        sections.forEach((section) => {
-            gsap.to(section, { autoAlpha: 1,
+        sections.forEach(section => {
+            gsap.to(section, {
+                autoAlpha: 1,
                 scrollTrigger: {
                     trigger: section,
                     start: "+=0 80%",
                     scrub: false,
                     markers: false,
-                    toggleActions: "play reverse play reverse"
-                }
+                    toggleActions: "play reverse play reverse",
+                },
             });
         });
-
-    },[])
+    }, []);
     return (
         <BlogGridContainer>
-            {
-                posts && posts.map((post, index) => {
-                    
-                    const featured = post.blogPost.featured === null ?
-                        false :
-                        post.blogPost.featured;
-                    
+            {posts &&
+                posts.map((post, index) => {
+                    const featured =
+                        post.blogPost.featured === null
+                            ? false
+                            : post.blogPost.featured;
+
                     const category = post.categories.nodes[0].name;
 
                     return (
-                            <BlogTile key={index} className="fadein" featured={featured} background={post.featuredImage.node.mediaItemUrl}>
-                            { featured && ( <p className="xs-heading uppercase white">{category}</p> ) }
+                        <BlogTile
+                            key={index}
+                            className="fadein"
+                            featured={featured}
+                            background={post.featuredImage.node.mediaItemUrl}>
+                            {featured && (
+                                <p className="xs-heading uppercase white">
+                                    {category}
+                                </p>
+                            )}
                             <Link href={post.uri}>
-                                <Image src={post.featuredImage.node.mediaItemUrl} width={500} height={436} layout="intrinsic" alt={post.featuredImage.node.altText} className="blogImage" />
+                                <Image
+                                    src={post.featuredImage.node.mediaItemUrl}
+                                    width={500}
+                                    height={436}
+                                    layout="intrinsic"
+                                    alt={post.featuredImage.node.altText}
+                                    className="blogImage"
+                                />
                             </Link>
-                            { !featured && ( <p className="xs-heading uppercase black">{category}</p> ) }
+                            {!featured && (
+                                <p className="xs-heading uppercase black">
+                                    {category}
+                                </p>
+                            )}
                             <Link href={post.uri}>
-                            <BlogTitle className="serif heading black">{post.title}</BlogTitle>
+                                <BlogTitle
+                                    className={`serif ${
+                                        featured ? "heading" : "press-heading"
+                                    } black`}>
+                                    {post.title}
+                                </BlogTitle>
                             </Link>
-                            </BlogTile>
-                    )
-                })
-            }
+                        </BlogTile>
+                    );
+                })}
         </BlogGridContainer>
-    )
+    );
 }
