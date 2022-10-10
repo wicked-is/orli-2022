5c54c3c1c5b25fe6b53eb6d05cd84822cb85be06
kennethtaylor <taylor.kennethg@gmail.com>
2022-10-10 12:28:45 -0700

    Cleanup/formatting

--- components/layout.js
+++ components/layout.js
@@ -4,14 +4,17 @@ import Header from "../components/header";
 import Gate from "../components/Gate";
 
 export default function Layout(props) {
-    const [isLoggedIn, setisLoggedIn] = useState(false)
-    return ( 
-        <div className={`${props.topBar?.isAnnouncementBarActive === undefined ? 'tob-bar-not-active' : 'top-bar-active'}`}>
+    const [isLoggedIn, setisLoggedIn] = useState(false);
+    return (
+        <div
+            className={`${
+                props.topBar?.isAnnouncementBarActive === undefined
+                    ? "tob-bar-not-active"
+                    : "top-bar-active"
+            }`}>
             <Header navItems={props.navItems} topBar={props.topBar} />
-            <main>
-                {props.children}
-            </main>
+            <main>{props.children}</main>
             <Footer footerImages={props.footerImages} />
         </div>
-    )
+    );
 }
