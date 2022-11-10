8e09f924d56eb83c8008e19d38aa0a31dad43c56
kennethtaylor <taylor.kennethg@gmail.com>
2022-11-10 14:24:31 -0800

    fix: aria label for image links

--- components/fauxSocialFeed.js
+++ components/fauxSocialFeed.js
@@ -1,56 +1,69 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/fauxSocialFeed.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/fauxSocialFeed.module.css";
 
 export default function FauxSocialFeed(props) {
-
-    
-    let headline = props.headline || 'Follow Along';
-    let backgroundColor = props.backgroundColor || 'Grey';
-    let ctaLink = props.ctaLink || 'https://www.instagram.com/stayorli';
-    let ctaText = props.ctaText || '@StayOrli';
+    let headline = props.headline || "Follow Along";
+    let backgroundColor = props.backgroundColor || "Grey";
+    let ctaLink = props.ctaLink || "https://www.instagram.com/stayorli";
+    let ctaText = props.ctaText || "@StayOrli";
     let images = props.image || [
         {
-            "altText": "",
-            "mediaItemUrl": "https://orlidev.wpengine.com/wp-content/uploads/2022/07/Orli_Post01.jpg"
+            altText: "",
+            mediaItemUrl:
+                "https://orlidev.wpengine.com/wp-content/uploads/2022/07/Orli_Post01.jpg",
         },
         {
-            "altText": "",
-            "mediaItemUrl": "https://orlidev.wpengine.com/wp-content/uploads/2022/07/ocean-view-3Edited2.jpg"
+            altText: "",
+            mediaItemUrl:
+                "https://orlidev.wpengine.com/wp-content/uploads/2022/07/ocean-view-3Edited2.jpg",
         },
         {
-            "altText": "",
-            "mediaItemUrl": "https://orlidev.wpengine.com/wp-content/uploads/2022/08/Orli_Recorder-scaled.jpg"
+            altText: "",
+            mediaItemUrl:
+                "https://orlidev.wpengine.com/wp-content/uploads/2022/08/Orli_Recorder-scaled.jpg",
         },
         {
-            "altText": "Orli La Jolla California Cold Brew Bar",
-            "mediaItemUrl": "https://orlidev.wpengine.com/wp-content/uploads/2022/07/Orli_Post10.jpg"
-        }
+            altText: "Orli La Jolla California Cold Brew Bar",
+            mediaItemUrl:
+                "https://orlidev.wpengine.com/wp-content/uploads/2022/07/Orli_Post10.jpg",
+        },
     ];
 
     return (
-        <section className={`${styles.fauxSocialFeedContainer} ${backgroundColor}-bg fauxsocial`}>
+        <section
+            className={`${styles.fauxSocialFeedContainer} ${backgroundColor}-bg fauxsocial`}>
             <div className={`${styles.max80}`}>
                 <div className={styles.topContainer}>
                     <h2 className="heading">{headline}</h2>
                     <Link href={ctaLink} passHref>
-                        <a className="sans-serif xs-copy underline" target="_blank" rel="noreferrer">{ctaText}</a>
+                        <a
+                            className="sans-serif xs-copy underline"
+                            target="_blank"
+                            rel="noreferrer">
+                            {ctaText}
+                        </a>
                     </Link>
                 </div>
                 <div className={styles.grid}>
-                    {
-                        images.map((image, index) => {
-                            return (
-                                <Link href={ctaLink} passHref key={`${index}-img`}>
-                                    <a target="_blank">           
-                                        <Image key={`faux-${index}`} src={image.mediaItemUrl} width={348} height={348} layout="responsive" alt={image.altText} />
-                                    </a>
-                                </Link>
-                            )
-                        })
-                    }
+                    {images.map((image, index) => {
+                        return (
+                            <Link href={ctaLink} passHref key={`${index}-img`}>
+                                <a target="_blank" aria-label="Orli Instagram">
+                                    <Image
+                                        key={`faux-${index}`}
+                                        src={image.mediaItemUrl}
+                                        width={348}
+                                        height={348}
+                                        layout="responsive"
+                                        alt={image.altText}
+                                    />
+                                </a>
+                            </Link>
+                        );
+                    })}
                 </div>
             </div>
         </section>
-    )
+    );
 }
