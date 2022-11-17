fe249fc88efa7db89cfb73de902a5aead384692b
kennethtaylor <taylor.kennethg@gmail.com>
2022-11-17 15:54:23 -0800

    update: adding gtm

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,9 +1,8 @@
-import Script from "next/script"
+import Script from "next/script";
 
 export default function Analytics(props) {
-
     // const EXAMPLE_UA_CODE = "UA-130029095-1"
-    const UA_CODE = "UA-234602062-1"
+    const UA_CODE = "UA-234602062-1";
 
     return (
         !props.siteNotLive && (
@@ -21,7 +20,14 @@ export default function Analytics(props) {
                         gtag('config', '${UA_CODE}');
                     `}
                 </Script>
+
+                <Script
+                    strategy="afterInteractive"
+                    id="tag-manager"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PMRKR2L');`,
+                    }}></Script>
             </>
         )
-    )
+    );
 }
