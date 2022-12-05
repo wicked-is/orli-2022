25d7b73eafe1bd4cffc5bb676dc9e471224bf94a
kennethtaylor <taylor.kennethg@gmail.com>
2022-12-05 14:53:54 -0800

    add: ClickEase

--- components/Analytics.js
+++ components/Analytics.js
@@ -35,6 +35,16 @@ export default function Analytics(props) {
                         __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '676429500584234');fbq('track', 'PageView');`,
                     }}
                 />
+                <Script
+                    strategy="afterInteractive"
+                    id="ClickCease"
+                    dangerouslySetInnerHTML={{
+                        __html: `var script = document.createElement('script');
+                            script.async = true; script.type = 'text/javascript';
+                            var target = 'https://www.clickcease.com/monitor/stat.js';
+                            script.src = target;var elem = document.head;elem.appendChild(script);`
+                    }}
+                />
             </>
         )
     );
