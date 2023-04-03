a0706cc08983ce1955821126ca1b81550eae8d70
kennethtaylor <taylor.kennethg@gmail.com>
2023-04-03 10:48:10 -0700

    Update bookingForm.js

--- components/bookingForm.js
+++ components/bookingForm.js
@@ -45,7 +45,7 @@ export default function BookingForm() {
 		Mews.Distributor(
 			{
 				// This is the demo id
-				configurationIds: ["93e27b6f-cba7-4e0b-a24a-819e1b7b388a"],
+				configurationIds: ["e12243c4-2c54-4d1c-a958-afb801279497"],
 				// This is the id from the Mews General Settings of the dashboard
 				// configurationIds: ["b6f6f212-71c7-4e05-897a-afb801278392"],
 				// This is the id from the Mews Booking Engines Settings of the dashboard
@@ -57,9 +57,6 @@ export default function BookingForm() {
 				// set different start and end date
 				api.setStartDate(new Date(checkInDate));
 				api.setEndDate(new Date(checkOutDate));
-			},
-			{
-				dataBaseUrl: "https://api.mews-demo.com",
 			}
 		);
 	}
@@ -76,8 +73,9 @@ export default function BookingForm() {
 				<form
 					className={styles.form}
 					method="POST"
-					action={`https://app.mews.com/distributor/b6f6f212-71c7-4e05-897a-afb801278392?mewsStart=${checkInDate}&mewsEnd=${checkOutDate}`}
-					target="_blank">
+					// action={`https://app.mews.com/distributor/b6f6f212-71c7-4e05-897a-afb801278392?mewsStart=${checkInDate}&mewsEnd=${checkOutDate}`}
+					onSubmit={handleFormSubmit}
+					target="">
 					{/* <form
 					className={styles.form}
 					method="POST"
