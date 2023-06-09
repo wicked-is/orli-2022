6850cb09120527fb17290ad7fa75e9c5d8e675bb
kennethtaylor <taylor.kennethg@gmail.com>
2023-06-09 14:04:08 -0700

    Update ContentBlock.js

--- components/ContentBlock.js
+++ components/ContentBlock.js
@@ -3,83 +3,85 @@ import parse from "html-react-parser";
 
 const ContentBlockMainContainer = styled.section``;
 const ContentBlockInnerContainer = styled.div`
-    width: 100%;
-    max-width: 960px;
-    margin-inline: auto;
-    margin-top: 5rem;
-    margin-bottom: 6rem;
+	width: 100%;
+	max-width: 960px;
+	margin-inline: auto;
+	margin-top: 5rem;
+	margin-bottom: 6rem;
 
-    @media screen and (max-width: 1050px) {
-        padding: 0 2rem;
-    }
+	@media screen and (max-width: 1050px) {
+		padding: 0 2rem;
+	}
 
-    h1,
-    h2,
-    h3,
-    h4 {
-        font-size: var(--heading);
-        font-family: essonnes-display, serif;
-        font-weight: 300;
-        line-height: 1.2;
-        font-style: normal;
-        padding-top: 2rem;
-        margin-block-start: 0em;
-        margin-block-end: 0em;
-    }
+	h1,
+	h2,
+	h3,
+	h4 {
+		font-size: var(--heading);
+		font-family: essonnes-display, serif;
+		font-weight: 300;
+		line-height: 1.2;
+		font-style: normal;
+		padding-top: 2rem;
+		margin-block-start: 0em;
+		margin-block-end: 0em;
+	}
 
-    ol li, 
-    ul li {
-        font-family: "GT Walsheim Light";
-        color: var(--black);
-        font-style: normal;
-        font-size: var(--body-copy);
-        line-height: 150%;
-        padding: 0 0 1rem 0;
-    }
+	ol li,
+	ul li {
+		font-family: "GT Walsheim Light";
+		color: var(--black);
+		font-style: normal;
+		font-size: var(--body-copy);
+		line-height: 150%;
+		padding: 0 0 1rem 0;
+	}
 
-    p {
-        font-size: var(--body-copy);
-        font-family: "GT Walsheim Light";
-        line-height: 150%;
-    }
-    a, a:visited, a:focus {
-        color: var(--brown); 
-        text-decoration: underline;
-    }
-    img {
-        max-width: 100%;
-        height: 100%;
+	p {
+		font-size: var(--body-copy);
+		font-family: "GT Walsheim Light";
+		line-height: 150%;
+	}
+	a,
+	a:visited,
+	a:focus {
+		/* color: var(--brown);  */
+		text-decoration: underline;
+	}
+	img {
+		max-width: 100%;
+		height: 100%;
 
-        &.aligncenter {
-            display: block;
-            margin: 0 auto;
-        }
-    }
+		&.aligncenter {
+			display: block;
+			margin: 0 auto;
+		}
+	}
 
-    figure {
-        width: 100%;
-        max-width: 100%;
-        margin: 0;
-    }
+	figure {
+		width: 100%;
+		max-width: 100%;
+		margin: 0;
+	}
 
-    @media only screen and (max-width: 700px) {
-        ol {
-            margin-block-start: 0em;
-            margin-block-end: 0em;
-            padding-inline-start: 20px;
-        }
-    }
+	@media only screen and (max-width: 700px) {
+		ol {
+			margin-block-start: 0em;
+			margin-block-end: 0em;
+			padding-inline-start: 20px;
+		}
+	}
 `;
 // .aligncenter
 // image swap
 export default function ContentBlock(props) {
-    const { anchor, content } = props;
-    return (
-        <ContentBlockMainContainer>
-            <a name={anchor} id={anchor}></a>
-            <ContentBlockInnerContainer>
-                {parse(content)}
-            </ContentBlockInnerContainer>
-        </ContentBlockMainContainer>
-    );
+	const { anchor, content } = props;
+	return (
+		<ContentBlockMainContainer>
+			<a name={anchor} id={anchor}></a>
+			<ContentBlockInnerContainer>
+				{parse(content)}
+			</ContentBlockInnerContainer>
+		</ContentBlockMainContainer>
+	);
 }
