4ec0a061cb35bb50ee8b0874bb9facf2ad941d4d
kennethtaylor <taylor.kennethg@gmail.com>
2024-03-26 22:55:43 -0700

    Update Analytics.js

--- components/Analytics.js
+++ components/Analytics.js
@@ -35,13 +35,13 @@ export default function Analytics(props) {
 						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N3Z8NP4');`,
 					}}
 				/>
-				<Script
+				{/* <Script
 					strategy="afterInteractive"
 					id="tag-manager-3"
 					dangerouslySetInnerHTML={{
 						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TG8CZ4V');`,
 					}}
-				/>
+				/> */}
 				<Script
 					strategy="afterInteractive"
 					id="fb pixel"
