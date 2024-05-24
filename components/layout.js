0defa16448d3203259525fdd9cd5c2455d689613
kennethtaylor <taylor.kennethg@gmail.com>
2024-05-24 16:24:42 -0700

    add: sticky btn

--- components/layout.js
+++ components/layout.js
@@ -1,43 +1,59 @@
-import { useEffect, useState } from "react";
+import exitIntent from "exit-intent";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import ExitIntent from "../components/exitIntent";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import Gate from "../components/Gate";
-import exitIntent from "exit-intent";
-import ExitIntent from "../components/exitIntent";
+import OrliWebSticker from "../public/assets/images/OfferWebSticker.png";
 
 export default function Layout(props) {
-    const [showModal, setshowModal] = useState(false);
-    const [hasModalShown, sethasModalShown] = useState(false);
+	const [showModal, setshowModal] = useState(false);
+	const [hasModalShown, sethasModalShown] = useState(false);
 
-    if (typeof document !== "undefined") {
-        setTimeout(() => {
-            const removeExitIntent = exitIntent({
-                threshold: 10,
-                maxDisplays: 1,
-                eventThrottle: 200,
-                onExitIntent: () => {
-                    if (hasModalShown) {
-                        return;
-                    }
-                    setshowModal(true);
-                },
-            });
-        }, 20_000);
-    }
+	if (typeof document !== "undefined") {
+		setTimeout(() => {
+			const removeExitIntent = exitIntent({
+				threshold: 10,
+				maxDisplays: 1,
+				eventThrottle: 200,
+				onExitIntent: () => {
+					if (hasModalShown) {
+						return;
+					}
+					setshowModal(true);
+				},
+			});
+		}, 20_000);
+	}
 
-    return (
-        <div data-page={`${props.currentPage}`}
-             className={`${
-                props.topBar?.isAnnouncementBarActive === undefined
-                    ? "tob-bar-not-active"
-                    : "top-bar-active"
-            }`}>
-            <Header navItems={props.navItems} topBar={props.topBar} />
-            <main>{props.children}</main>
-            {showModal && !hasModalShown && props.page !== "/email" && (
-                <ExitIntent toggleModal={{ setshowModal, sethasModalShown }} />
-            )}
-            <Footer page={props.page} />
-        </div>
-    );
+	return (
+		<div
+			data-page={`${props.currentPage}`}
+			className={`${
+				props.topBar?.isAnnouncementBarActive === undefined
+					? "tob-bar-not-active"
+					: "top-bar-active"
+			}`}>
+			<Header navItems={props.navItems} topBar={props.topBar} />
+			<main>
+				<Link href="/offers" passHref>
+					<a
+						style={{
+							position: "fixed",
+							bottom: "1rem",
+							right: "1rem",
+							zIndex: 99,
+						}}>
+						<Image src={OrliWebSticker} width={100} height={100} />
+					</a>
+				</Link>
+				{props.children}
+			</main>
+			{showModal && !hasModalShown && props.page !== "/email" && (
+				<ExitIntent toggleModal={{ setshowModal, sethasModalShown }} />
+			)}
+			<Footer page={props.page} />
+		</div>
+	);
 }
