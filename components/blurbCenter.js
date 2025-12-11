13f218744067a67ed7518ef15ce65c474e6f8dfa
kennethtaylor <taylor.kennethg@gmail.com>
2025-12-11 09:35:06 -0800

    Update blurbCenter.js

--- components/blurbCenter.js
+++ components/blurbCenter.js
@@ -1,58 +1,69 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import styles from '../styles/blurbCenter.module.css';
+import styles from "../styles/blurbCenter.module.css";
+import parse from "html-react-parser";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function BlurbCenter(props) {
-    useEffect(() => {
-        var blurbcentersections = gsap.utils.toArray('.blurbcenterfade');
+	useEffect(() => {
+		var blurbcentersections = gsap.utils.toArray(".blurbcenterfade");
 
-        blurbcentersections.forEach((blurbcentersection) => {
-            gsap.to(blurbcentersection, { autoAlpha: 1,
-                scrollTrigger: {
-                    trigger: blurbcentersection,
-                    start: "+=0 80%",
-                    scrub: false,
-                    markers: false,
-                    toggleActions: "play reverse play reverse"
-                }
-            });
-        });
+		blurbcentersections.forEach((blurbcentersection) => {
+			gsap.to(blurbcentersection, {
+				autoAlpha: 1,
+				scrollTrigger: {
+					trigger: blurbcentersection,
+					start: "+=0 80%",
+					scrub: false,
+					markers: false,
+					toggleActions: "play reverse play reverse",
+				},
+			});
+		});
+	}, []);
 
-    },[])
+	const {
+		blurb,
+		greyBackground,
+		hasBackgroundMedia,
+		headline,
+		icon,
+		imagePoster,
+		mp4ExternalLink,
+		webm,
+		isBlogSummary,
+	} = props;
 
-    const {
-        blurb,
-        greyBackground,
-        hasBackgroundMedia,
-        headline,
-        icon,
-        imagePoster,
-        mp4ExternalLink,
-        webm,
-        isBlogSummary
-    } = props
-
-    return (
-        <section className={`${styles.blurbContainer} ${greyBackground && 'bg-lt-grey'} ${mp4ExternalLink ? 'has-video' : ''} ${isBlogSummary ? 'removepadding' : 'addpadding blurbcenterfade' }`}>
-            {mp4ExternalLink && (
-                <video poster={imagePoster.mediaItemUrl} autoPlay playsInline muted loop>
-                    <source src={mp4ExternalLink} type="video/mp4" />
-                    <source src={webm} type="video/webm" />
-                </video>
-            ) }
-            <div className="max-60 center">
-                { !mp4ExternalLink && <div className={`${styles.icon}`}>
-                    <img src={icon?.mediaItemUrl} alt={icon?.altText}/>
-                </div> }
-                {headline && ( 
-                    <p className="serif heading black center">{headline}</p>
-                )}
-                <p className="heading-italic">
-                    { blurb }
-                </p>
-            </div>
-        </section>
-    )
+	return (
+		<section
+			className={`${styles.blurbContainer} ${
+				greyBackground && "bg-lt-grey"
+			} ${mp4ExternalLink ? "has-video" : ""} ${
+				isBlogSummary ? "removepadding" : "addpadding blurbcenterfade"
+			}`}>
+			{mp4ExternalLink && (
+				<video
+					poster={imagePoster.mediaItemUrl}
+					autoPlay
+					playsInline
+					muted
+					loop>
+					<source src={mp4ExternalLink} type="video/mp4" />
+					<source src={webm} type="video/webm" />
+				</video>
+			)}
+			<div className="max-60 center">
+				{!mp4ExternalLink && (
+					<div className={`${styles.icon}`}>
+						<img src={icon?.mediaItemUrl} alt={icon?.altText} />
+					</div>
+				)}
+				{headline && (
+					<p className="serif heading black center">{headline}</p>
+				)}
+				<p className="heading-italic">{parse(blurb)}</p>
+			</div>
+		</section>
+	);
 }
