5175118e99b48c3783c9f2b228675b7dd7d47cbe
kennethtaylor <taylor.kennethg@gmail.com>
2026-01-15 13:22:40 -0800

    Update twoStaggeredImages.js

--- components/twoStaggeredImages.js
+++ components/twoStaggeredImages.js
@@ -1,20 +1,40 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import styles from '../styles/twoStaggeredImages.module.css';
+import styles from "../styles/twoStaggeredImages.module.css";
 
 export default function TwoStaggeredPhotos({ left, right }) {
-    return (
-        <section className={styles.photoContainer}>
-            <div className={styles.left}>
-                <Image src={left} width={561} height={370} layout="responsive" />
-            </div>
-            <div className={styles.right}>
-                {/* <Image src={right} width={708} height={434} layout="responsive" /> */}
-                <video poster={"https://orlidev.wpengine.com/wp-content/uploads/2022/09/Orli_Discoveries_RightWeb-1.jpg"} className={styles.videoBG} autoPlay playsInline muted loop>
-                    <source src="https://stayorli.com/wp-content/uploads/2022/02/orlihero5-1.mp4" type="video/mp4" />
-                    <source src="https://stayorli.com/wp-content/uploads/2022/02/orlihero5-1.webm" type="video/webm" />
-                </video>
-            </div>
-        </section>
-    )
+	return (
+		<section className={styles.photoContainer}>
+			<div className={styles.left}>
+				<Image
+					src={left}
+					width={561}
+					height={370}
+					layout="responsive"
+					alt="Left Image"
+				/>
+			</div>
+			<div className={styles.right}>
+				{/* <Image src={right} width={708} height={434} layout="responsive" /> */}
+				<video
+					poster={
+						"https://orlidev.wpengine.com/wp-content/uploads/2022/09/Orli_Discoveries_RightWeb-1.jpg"
+					}
+					className={styles.videoBG}
+					autoPlay
+					playsInline
+					muted
+					loop>
+					<source
+						src="https://stayorli.com/wp-content/uploads/2022/02/orlihero5-1.mp4"
+						type="video/mp4"
+					/>
+					<source
+						src="https://stayorli.com/wp-content/uploads/2022/02/orlihero5-1.webm"
+						type="video/webm"
+					/>
+				</video>
+			</div>
+		</section>
+	);
 }
