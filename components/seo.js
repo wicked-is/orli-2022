23e81d13f7f7458b3bcc4ad277c2fcccf6c23400
kennethtaylor <taylor.kennethg@gmail.com>
2026-04-19 15:36:01 -0700

    Update seo.js

--- components/seo.js
+++ components/seo.js
@@ -1,14 +1,15 @@
-import Head from "next/head"
-import parse from "html-react-parser"
+import Head from "next/head";
+import parse from "html-react-parser";
 
 export default function SEO(props) {
-    
-    const { title, description, fullhead } = props;
-    return (
-        <Head>
-            {title && !fullhead && <title>{title}</title>}
-            {description && !fullhead && <meta name="description" content={description} />}
-            {fullhead && parse(fullhead) }
-        </Head>
-    )
+	const { title, description, fullhead } = props;
+	return (
+		<Head>
+			{title && !fullhead && <title>{title}</title>}
+			{description && !fullhead && (
+				<meta name="description" content={description} />
+			)}
+			{fullhead && parse(fullhead.replace("/home", "/"))}
+		</Head>
+	);
 }
